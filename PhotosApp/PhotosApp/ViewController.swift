8fcfa31acfacc95b999c2a8287db4ed67bc9d0e9
corykim0829 <corykim0829@gmail.com>
2020-03-16 14:32:45 +0900

    Feat: Create a colelctionView

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
