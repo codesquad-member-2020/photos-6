a4427dcb3c08c4d19ddb29148550f218f502fc24
Dongjun Kim <34022757+corykim0829@users.noreply.github.com>
2020-03-18 12:41:55 +0900

    이미지 정보가 담긴 JSON을 디코딩하는 객체 구현 (#10)
    * [#7] Feat: DoodleDataManager - fetching & decoding JSON
    
    * [#7] Refactor: Sort swift files by their function
    
    * [#7] Refactor: Capitalize acronyms properties' name
    
    * [#7] Refactor: Remove print methods, Manage exception error in flow

---
+++ PhotosApp/PhotosApp/Extensions/DateFormatter.swift
@@ -0,0 +1,18 @@
+//
+//  DateFormatter.swift
+//  PhotosApp
+//
+//  Created by Cory Kim on 2020/03/18.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let doodleDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        return dateFormatter
+    }()
+}
---
+++ PhotosApp/PhotosApp/Models/DoodleDataManager.swift
@@ -0,0 +1,33 @@
+//
+//  DoodleDataManager.swift
+//  PhotosApp
+//
+//  Created by Cory Kim on 2020/03/18.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+class DoodleDataManager {
+    let doodleURL = "https://public.codesquad.kr/jk/doodle.json"
+    private var doodleImages: [DoodleImage]?
+    
+    init() {
+        decodeJSON()
+    }
+    
+    private func decodeJSON() {
+        guard let url = URL(string: doodleURL) else { return }
+        URLSession.shared.dataTask(with: url) { (data, _, err) in
+            guard err == nil else { self.doodleImages = nil; return }
+            guard let data = data else { self.doodleImages = nil; return }
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .formatted(.doodleDateFormatter)
+            do {
+                self.doodleImages = try decoder.decode([DoodleImage].self, from: data)
+            } catch {
+                self.doodleImages = nil
+            }
+        }
+    }
+}
