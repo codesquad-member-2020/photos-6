a27dde25199959fdbe0655edc85ac91b7780f476
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2020-03-19 23:30:37 +0900

    Long Press - Menu Item을 통한 이미지 저장 구현 (#16)
    * [#9] Resolve Conflict while cherry pick
    
    * [#9] Feat: save image by menu item
    
    - selected by long press gesture
    
    * [#9] Fix: target rect of selected cell
    
    * [#9] Refactor: remove unused method
    
    * [#9] Fix: Move code that stores images in the Photo Library
    
    - Move code from DataSource to CollectionViewController

--- PhotosApp/PhotosApp/Controllers/DoodleCollectionViewController.swift
+++ PhotosApp/PhotosApp/Controllers/DoodleCollectionViewController.swift
@@ -7,30 +7,41 @@
 //
 
 import UIKit
+import Photos
 
 class DoodleCollectionViewController: UICollectionViewController {
     
+    private var indexPathOfSelectedCell : IndexPath?
     private let navigationBarTitle = "Doodles"
     private let delegateFlowLayout = DoodleCollectionViewDelegateFlowLayout()
     private let dataSource = DoodleCollectionViewDataSource()
+    private var longPressGestureRecognizer: UILongPressGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCollectionView()
+        setupGestureRecognizer()
         setupNavigationBar()
         setupNotification()
         setupUI()
     }
     
     private func setupCollectionView() {
-        self.collectionView.dataSource = dataSource
-        self.collectionView.delegate = delegateFlowLayout
-        self.collectionView.register(DoodleImageCell.self, forCellWithReuseIdentifier: DoodleImageCell.identifier)
+        collectionView.dataSource = dataSource
+        collectionView.delegate = delegateFlowLayout
+        collectionView.register(DoodleImageCell.self, forCellWithReuseIdentifier: DoodleImageCell.identifier)
+        collectionView.backgroundColor = .darkGray
     }
-
+    
     private func setupNavigationBar() {
         navigationItem.title = navigationBarTitle
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(closeButtonTapped))
+    }
+    
+    private func setupGestureRecognizer() {
+        longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.handleLongPressGesture))
+        longPressGestureRecognizer.minimumPressDuration = 1
+        self.collectionView.addGestureRecognizer(longPressGestureRecognizer)
     }
     
     @objc func closeButtonTapped() {
@@ -58,5 +69,27 @@ class DoodleCollectionViewController: UICollectionViewController {
         NotificationCenter.default.removeObserver(self,
                                                   name: DoodleDataManager.DoodleImagesHaveDecodedNotification,
                                                   object: nil)
+        collectionView.removeGestureRecognizer(longPressGestureRecognizer)
+    }
+}
+
+extension DoodleCollectionViewController {
+    @objc func handleLongPressGesture(gesture: UILongPressGestureRecognizer){
+        let location = gesture.location(in: self.collectionView)
+        guard let indexPath = collectionView.indexPathForItem(at: location) else { return }
+        indexPathOfSelectedCell = indexPath
+        guard let selectedCell = collectionView.cellForItem(at: indexPath) else { return }
+        let menuItem = UIMenuItem(title: "Save", action: #selector(saveImage))
+        UIMenuController.shared.menuItems = [menuItem]
+        UIMenuController.shared.showMenu(from: selectedCell, rect: selectedCell.contentView.frame)
+        selectedCell.becomeFirstResponder()
+    }
+    
+    @objc func saveImage() {
+        guard let indexPath = indexPathOfSelectedCell else { return }
+        let cell = collectionView.cellForItem(at: indexPath) as! DoodleImageCell
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAsset(from: cell.doodleImage)
+        })
     }
 }
