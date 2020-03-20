7ec9c539008f9979bb5f56abfd782874bdea73aa
corykim0829 <corykim0829@gmail.com>
2020-03-20 11:04:53 +0900

    [#17] Feat: Display seelctedBackgroundView selecting PhotoCell

--- PhotosApp/PhotosApp/Views/PhotoCollectionViewDelegateFlowLayout.swift
+++ PhotosApp/PhotosApp/Views/PhotoCollectionViewDelegateFlowLayout.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class PhotoCollectionViewDelegateFlowLayout: NSObject, UICollectionViewDelegateFlowLayout {
     
-    let cellSize = CGSize(width: 100, height: 100)
+    private let numberOfItemsInRow: CGFloat = 3
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return cellSize
+        let width = (collectionView.frame.size.width - ViewController.minimumItemSpacing * 2) / numberOfItemsInRow
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! PhotoCell
+        cell.selectedBackgroundView = {
+            let backgroundView = UIView()
+            backgroundView.layer.borderColor = #colorLiteral(red: 0.2784313725, green: 0.7176470588, blue: 0.5294117647, alpha: 1)
+            backgroundView.layer.borderWidth = 4
+            backgroundView.backgroundColor = .clear
+            return backgroundView
+        }()
+        cell.bringSubviewToFront(cell.selectedBackgroundView!)
     }
 }
